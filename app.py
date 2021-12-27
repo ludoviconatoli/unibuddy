927d1ad0d766052da27fa671eaead49587b88c04
ludov <ludovico.natoli@gmail.com>
2021-12-27 12:06:02 +0100

    fine database

--- app.py
+++ app.py
@@ -1,37 +1,46 @@
-from flask import Flask, session, redirect, url_for
+import re
+
+from flask import Flask, session, redirect, url_for, flash
 from flask import render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email, ValidationError
 
 from project import app
+from project.models import Student
+
 
 @app.route('/')
-def index():  # put application's code here
+def index():
     return render_template("start.html")
 
 class FormLogin(FlaskForm):
-    username = StringField("email", validators=[DataRequired()])
-    password = PasswordField("password")
-    submit = SubmitField("Submit")
+    username = StringField("email", validators=[DataRequired(), Email()])
+    password = PasswordField("password", validators=[DataRequired()])
+    submit = SubmitField("Sign in")
 
-@app.route('/login/', methods=["GET", "POST"])
+@app.route('/login/', methods=["POST", "GET"])
 def login():
-    user = False
-    password = False
-    form = FormLogin()
+    login_form = FormLogin()
 
-    if form.validate_on_submit():
-        session["username"] = form.username.data
-        session["password"] = form.password.data
+    if login_form.validate_on_submit():
+        user_email = Student.query.filter_by(email=login_form.email.data).first()
+        if user_email:
+            session['email'] = user_email.email
+            session['student_id'] = user_email.student_id
+            session['logged'] = True
+            session['name'] = user_email.name
+            session['surname'] = user_email.surname
+            session['study_course'] = user_email.study_course
+            session['university'] = user_email.university
+            return redirect(url_for('index'))
 
-        form.username.data = ""
-        form.password.data = ""
+    return render_template("login.html", login_form=login_form)
 
-        return redirect(url_for("index"))
-
-    return render_template("login.html", login_form=form, username=user, password=password)
-
+@app.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('start.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
